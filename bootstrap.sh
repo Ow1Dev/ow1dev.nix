9dab764145510b18c5d65e2beec82e6d86cb05ef
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-02-17 23:10:22 +0100

    feat: adding bootstrap to file

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if ! command -v nix &> /dev/null; then
+  curl -L https://nixos.org/nix/install | sh
+  . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+fi
+
+nix develop --command just switch
+
+DOTFILES_DIR="$HOME/.dotfiles"
+if [ -d "$DOTFILES_DIR" ]; then
+  git -C "$DOTFILES_DIR" pull
+else
+  git clone https://github.com/YOUR_USERNAME/dotfiles.git "$DOTFILES_DIR"
+fi
+
+cd "$DOTFILES_DIR"
+just switch
+
+source ~/.bashrc || source ~/.zshrc || source ~/.profile
