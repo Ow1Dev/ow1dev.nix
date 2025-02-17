4dd142eeafc44be1b48e74d56b30f17b0bef6928
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-02-17 23:11:48 +0100

    feat: adding correct github url

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ DOTFILES_DIR="$HOME/.dotfiles"
 if [ -d "$DOTFILES_DIR" ]; then
   git -C "$DOTFILES_DIR" pull
 else
-  git clone https://github.com/YOUR_USERNAME/dotfiles.git "$DOTFILES_DIR"
+  git clone https://github.com/Ow1Dev/ow1dev.nix "$DOTFILES_DIR"
 fi
 
 cd "$DOTFILES_DIR"
