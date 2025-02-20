1c8152bc2a5cdf458f313e9072217a3032d0555d
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-02-20 21:42:41 +0100

    fix: change scan

--- lib/default.nix
+++ lib/default.nix
@@ -1,20 +1,18 @@
-# FIXME(lib.custom): Add some stuff from hmajid2301/dotfiles/lib/module/default.nix, as simplifies option declaration
 { lib, ... }:
-{
-  # use path relative to the root of the project
-  relativeToRoot = lib.path.append ../.;
-  scanPaths =
-    path:
-    builtins.map (f: (path + "/${f}")) (
-      builtins.attrNames (
-        lib.attrsets.filterAttrs (
-          path: _type:
-          (_type == "directory") # include directories
-          || (
-            (path != "default.nix") # ignore default.nix
-            && (lib.strings.hasSuffix ".nix" path) # include .nix files
-          )
-        ) (builtins.readDir path)
+
+let
+  # Helper function to get paths
+  getScanPaths = path: let
+    # Ensure we are not adding paths back to the root directory or looping over itself
+    filteredPaths = builtins.filter (p: p != path) (builtins.attrNames (lib.attrsets.filterAttrs (
+      path' : _type: (_type == "directory") || (
+        (path' != "default.nix") && (lib.strings.hasSuffix ".nix" path')
       )
-    );
+    ) (builtins.readDir path)));
+  in builtins.map (f: path + "/${f}") filteredPaths;
+in
+{
+  relativeToRoot = lib.path.append ../.;
+  scanPaths = getScanPaths;
 }
+
