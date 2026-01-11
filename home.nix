6a0a09f668bb6a53ad67208d1b7a1b667255dbec
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2026-01-11 15:47:48 +0100

    feat: adding nnn to easier navigation

--- home.nix
+++ home.nix
@@ -19,6 +19,7 @@
   home.packages = with pkgs; [
     gh
     jq
+    nnn
   ];
 
   home.sessionVariables = {
--- programs/zsh.nix
+++ programs/zsh.nix
@@ -9,6 +9,26 @@
     enableCompletion = true;
     dotDir = "${config.xdg.configHome}/zsh";
     history.path = "${config.home.homeDirectory}/.config/zsh/.zsh_history";
+    initContent = ''
+    n () {
+      if [ -n $NNNLVL ] && [ "$NNNLVL" -ge 1 ]; then
+        echo "nnn is already running"
+        return
+      fi
+
+      export NNN_TMPFILE="$HOME/.config/nnn/.lastd"
+
+      nnn -adeHo "$@"
+
+      if [ -f "$NNN_TMPFILE" ]; then
+        . "$NNN_TMPFILE"
+        rm -f "$NNN_TMPFILE" > /dev/null
+      fi
+    }
+    '';
+    shellAliases = {
+      ll = "n";
+    };
   };
 
   programs.starship = {
